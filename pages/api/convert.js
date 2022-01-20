549e912df28a4a211b7ad1da952a96c6d5a8eb03
GABRIEL SANTOS LIMA <lima.gabriel@LELLO.CORP>
2022-01-20 08:45:53 -0300

    s-2220

--- pages/api/convert.js
+++ pages/api/convert.js
@@ -14,14 +14,59 @@ async function getValeusToCsv(json, type = 'default') {
   const sanitize = {
     'S-2220'(input) {
       let output = input;
-      return output;
+      return new Promise((resolve, reject) => {
+        input.eSocial.evtMonit[0]?.exMedOcup.forEach(atendimento => {
+          let inscricao =  input.eSocial.evtMonit[0]?.ideEmpregador[0]?.nrInsc ?? 'InscricaoNaoIdentificada';
+          if (!atendimento?.respMonit) {
+            atendimento.respMonit = [{}]
+          }
+
+          if (!atendimento?.aso) {
+            atendimento.aso = [{
+              medico: [{}]
+            }]
+          }
+
+          let promises = [{
+            ideeve: input.eSocial.evtMonit[0]?.$.Id,
+            tipins: input.eSocial.evtMonit[0]?.ideEmpregador[0]?.tpInsc ?? [''],
+            numins: input.eSocial.evtMonit[0]?.ideEmpregador[0]?.nrInsc ?? [''],
+            cateso: input.eSocial.evtMonit[0]?.ideVinculo[0]?.codCateg ?? [''],
+            cpftra: input.eSocial.evtMonit[0]?.ideVinculo[0]?.cpfTrab ?? [''],
+            indret: input.eSocial.evtMonit[0]?.ideEvento[0]?.indRetif ?? [''],
+            tipaso: atendimento?.tpExameOcup ?? [''],
+            dataso: atendimento.aso[0]?.dtAso ? new Date(atendimento.aso[0]?.dtAso[0]).toLocaleDateString('pt-BR') : [''],
+            resaso: atendimento.aso[0]?.resAso ?? [''],
+            nommed: atendimento.aso[0]?.medico[0]?.nmMed ?? [''],
+            nrcrmm: atendimento.aso[0]?.medico[0]?.nrCRM ?? [''],
+            ufcrmm: atendimento.aso[0]?.medico[0]?.ufCRM ?? [''],
+            cpfrsp: atendimento.respMonit[0]?.cpfResp ?? [''],
+            nomrsp: atendimento.respMonit[0]?.nmResp ?? [''],
+          }].map(async info => {
+            return await getValeusToCsv(info);
+          })
+
+          Promise.allSettled(promises)
+            .then(result => {
+              tables = result.map(process => ({ ...process.value.conteudo, name: 'ASO_' + inscricao + `_${new Date().toISOString().split('.')[0]?.replace('T', '_').replace(':', 'H').replace(':', 'M') + 'S'}` }));
+              resolve(output);
+
+            }).catch(error => {
+              console.log({ error });
+              resolve(output);
+
+            });
+
+        })
+
+      });
     },
     async 'S-2240'(input) {
       let output = input;
 
       return new Promise((resolve, reject) => {
         input.eSocial.evtExpRisco.forEach(risco => {
-          let incricao = risco.ideEmpregador[0].nrInsc[0];
+          let incricao = risco.ideEmpregador[0]?.nrInsc[0];
 
           let promises = risco.infoExpRisco.map(async info => {
             return await getValeusToCsv(info);
@@ -30,7 +75,7 @@ async function getValeusToCsv(json, type = 'default') {
 
           Promise.allSettled(promises)
             .then(result => {
-              tables = result.map(process => ({ ...process.value.conteudo, name: 'AN_' + incricao + `_${new Date().toISOString().split('.')[0].replace('T', '_').replace(':', 'H').replace(':', 'M') + 'S'}` }));
+              tables = result.map(process => ({ ...process.value.conteudo, name: 'AN_' + incricao + `_${new Date().toISOString().split('.')[0]?.replace('T', '_').replace(':', 'H').replace(':', 'M') + 'S'}` }));
               resolve(output);
 
             }).catch(error => {
